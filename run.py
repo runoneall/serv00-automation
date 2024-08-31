e5e2894a7563672d1562af61ffa3b23cee4caee6
runoneall <134772645+runoneall@users.noreply.github.com>
2024-08-31 16:03:00 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,84 +1,40 @@
 import os
 import paramiko
 import requests
-import json
-from datetime import datetime, timezone, timedelta
+import ast
 
-def ssh_multiple_connections(hosts_info, command):
-    users = []
-    hostnames = []
-    for host_info in hosts_info:
-        hostname = host_info['hostname']
-        username = host_info['username']
-        password = host_info['password']
-        try:
-            ssh = paramiko.SSHClient()
-            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            ssh.connect(hostname=hostname, port=22, username=username, password=password)
-            stdin, stdout, stderr = ssh.exec_command(command)
-            user = stdout.read().decode().strip()
-            users.append(user)
-            hostnames.append(hostname)
-            ssh.close()
-        except Exception as e:
-            print(f"用户：{username}，连接 {hostname} 时出错: {str(e)}")
-    return users, hostnames
+# get env
+SSH_INFO = ast.literal_eval(os.getenv('SSH_INFO', '[]'))
+COMMAND = ast.literal_eval(os.getenv('COMMAND', '[]'))
+PUSHURL = os.getenv('PUSHURL', '')
 
-ssh_info_str = os.getenv('SSH_INFO', '[]')
-hosts_info = json.loads(ssh_info_str)
+# ssh login
+SSHClient = paramiko.SSHClient()
+SSHClient.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+Log = str()
+for Item in SSH_INFO:
+    print('Connect Server ...')
+    Log += f"---\nConnect Server: {Item['hostname']}\n"
+    SSHClient.connect(Item['hostname'], username=Item['username'], password=Item['password'])
+    print('Run Command ...')
+    for Line in COMMAND:
+        Log += f'Run Command: {Line}\n'
+        stdin, stdout, stderr = SSHClient.exec_command(Line)
+        output = stdout.read().decode('utf-8')
+        error = stderr.read().decode('utf-8')
+        if output:
+            Log += f'Output: \n```\n{output}\n```\n'
+        if error:
+            Log += f'Error: \n```\n{error}\n```\n'
 
-command = 'whoami'
-user_list, hostname_list = ssh_multiple_connections(hosts_info, command)
-user_num = len(user_list)
-content = "SSH服务器登录信息：\n"
-for user, hostname in zip(user_list, hostname_list):
-    content += f"用户名：{user}，服务器：{hostname}\n"
-beijing_timezone = timezone(timedelta(hours=8))
-time = datetime.now(beijing_timezone).strftime('%Y-%m-%d %H:%M:%S')
-menu = requests.get('https://api.zzzwb.com/v1?get=tg').json()
-loginip = requests.get('https://api.ipify.org?format=json').json()['ip']
-content += f"本次登录用户共： {user_num} 个\n登录时间：{time}\n登录IP：{loginip}"
-
-push = os.getenv('PUSH')
-
-def mail_push(url):
-    data = {
-        "body": content,
-        "email": os.getenv('MAIL')
-    }
-
-    response = requests.post(url, json=data)
-
-    try:
-        response_data = json.loads(response.text)
-        if response_data['code'] == 200:
-            print("推送成功")
-        else:
-            print(f"推送失败，错误代码：{response_data['code']}")
-    except json.JSONDecodeError:
-        print("连接邮箱服务器失败了")
-
-def telegram_push(message):
-    url = f"https://api.telegram.org/bot{os.getenv('TELEGRAM_BOT_TOKEN')}/sendMessage"
-    payload = {
-        'chat_id': os.getenv('TELEGRAM_CHAT_ID'),
-        'text': message,
-        'parse_mode': 'HTML',
-        'reply_markup': json.dumps({
-            "inline_keyboard": menu,
-            "one_time_keyboard": True
-         })
-    }
-    headers = {
-        'Content-Type': 'application/json'
-    }
-    response = requests.post(url, json=payload, headers=headers)
-    if response.status_code != 200:
-        print(f"发送消息到Telegram失败: {response.text}")
-
-if push == "mail":
-    mail_push('https://zzzwb.us.kg/test')
-elif push == "telegram":
-    telegram_push(content)
-else:
-    print("推送失败，推送参数设置错误")
+# yunhu push
+print('Push Response ...')
+req_data = {
+    'from': 'Github Serv00 Auto Login Action',
+    'to': 2625640,
+    'type': 'user',
+    'content': f'Auto Login Response\n{Log}'
+}
+rep_data = requests.post(PUSHURL, json=req_data)
+rep_data = rep_data.json()
+print(rep_data['message'])
