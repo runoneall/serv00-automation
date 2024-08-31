6fe94cff5d6069ed55a508a1568c3769932f3867
runoneall <134772645+runoneall@users.noreply.github.com>
2024-08-31 19:25:18 +0800

    update run.py

--- run.py
+++ run.py
@@ -3,6 +3,18 @@ import paramiko
 import requests
 import ast
 
+# yunhu push function
+def YunhuPush(Content:str):
+    req_data = {
+        'from': 'Github Serv00 Auto Login Action',
+        'to': USERID,
+        'type': USERTYPE,
+        'content': f'Auto Login Response\n{Content}'
+    }
+    rep_data = requests.post(PUSHURL, json=req_data)
+    rep_data = rep_data.json()
+    print(rep_data['message'])
+
 # get env
 SSH_INFO = ast.literal_eval(os.getenv('SSH_INFO', '[]'))
 COMMAND = ast.literal_eval(os.getenv('COMMAND', '[]'))
@@ -17,7 +29,12 @@ Log = str()
 for Item in SSH_INFO:
     print('Connect Server ...')
     Log += f"---\nConnect Server: `{Item['hostname']}`\n"
-    SSHClient.connect(Item['hostname'], username=Item['username'], password=Item['password'])
+    try:
+        SSHClient.connect(Item['hostname'], username=Item['username'], password=Item['password'], timeout=180)
+    except:
+        print('!!! Connect Failed !!!')
+        YunhuPush(Content='# !!! Connect Failed !!!')
+        exit()
     print('Run Command ...')
     for Line in COMMAND:
         Log += f'Run Command: `{Line}`\n'
@@ -31,12 +48,4 @@ for Item in SSH_INFO:
 
 # yunhu push
 print('Push Response ...')
-req_data = {
-    'from': 'Github Serv00 Auto Login Action',
-    'to': USERID,
-    'type': USERTYPE,
-    'content': f'Auto Login Response\n{Log}'
-}
-rep_data = requests.post(PUSHURL, json=req_data)
-rep_data = rep_data.json()
-print(rep_data['message'])
+YunhuPush(Content=Log)
